2d782db9319837c4e92d1e327f03b6e41a24bb1d
nshahpazov <nshahpazov@gmail.com>
2018-06-25 11:11:57 +0300

    board-component is showing columns and real data now

--- src/components/BoardComponent.ts
+++ src/components/BoardComponent.ts
@@ -1,44 +1,46 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { TodoItem } from '../entity/TodoItem';
-import { Category } from '../entity/Category';
-import { Author } from '../entity/Author';
+import { User } from '../entity/User';
+import { Board } from '../entity/Board';
+import { TaskColumn } from '../entity/TaskColumn';
+
+import { BoardService } from '../services/BoardService';
 
 @Component({
+  props: {
+    board: Board
+  },
   template: `
-  <div>
-    <input v-model="todoText"/><button @click="onClick">Create New Todo!</button>
+  <div v-if="board">
     <div>
-      <p>{{message}}</p>
-      <div v-for="todo in todos">{{todo.title}}</div>
-      </div>
+       NAME: {{ board.name }}
+       <input type="text" v-model="newColumnName" />
+       <button v-on:click="addColumn">
+         Add Column
+       </button>
+       <div v-for="column in board.columns">
+        {{ column.name }}
+       </div>
+    </div>
   </div>
   `
 })
 export default class BoardComponent extends Vue {
-  // Initial data can be declared as instance properties
-  message: string = 'BoardComponent!';
-  todoText: string = '';
-  todos: Array<TodoItem> = [];
-  repository;
 
-  async created() {
-    const repository = this.connection.getRepository(TodoItem);
-    this.todos = await repository.find();
-    console.log(this.todos);
-    debugger;
+  board: Board;
+  newColumnName = '';
+  boardService: BoardService;
+
+  constructor() {
+    super();
+    this.boardService = new BoardService(this.connection);
   }
 
-  // Component methods can be declared as instance methods
-  async onClick() {
-    const repository = this.connection.getRepository(TodoItem);
+  async addColumn() {
+    const column = new TaskColumn();
+    column.name = this.newColumnName;
+    this.board.columns.push(column);
 
-    const todoItem = new TodoItem();
-    todoItem.text = `Some text`;
-    todoItem.title = `Some title`;
-
-    await repository.save(todoItem);
-
-    window.alert(this.todoText);
+    await this.boardService.save(this.board);
   }
 }
